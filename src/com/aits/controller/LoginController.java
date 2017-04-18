fda8d1a2b944d32a9a04f15076e9a7861d4cc3ea
santosh <kshirsagarsantosh44@gmail.com>
2017-04-18 15:07:42 +0530

    initial co

---
+++ src/com/aits/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.aits.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.aits.model.Student;
+
+@Controller
+public class LoginController {
+	@RequestMapping(value="loginStudent",method=RequestMethod.GET)
+	public String loginStudent(Model model){
+		/*Student student=new Student();
+		model.addAttribute("student",student);*/
+		return "loginForm";
+	}
+	
+	@RequestMapping(value = "/accessdenied", method = RequestMethod.GET)
+    public String loginerror(Model model) {
+        model.addAttribute("error", "true");
+        return "denied";
+    }
+}
---
+++ src/com/aits/util/Test.java
@@ -0,0 +1,27 @@
+package com.aits.util;
+
+import javax.validation.Configuration;
+import javax.validation.ValidatorFactory;
+import javax.validation.spi.BootstrapState;
+import javax.validation.spi.ConfigurationState;
+import javax.validation.spi.ValidationProvider;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class Test implements Validator{
+
+	@Override
+	public boolean supports(Class<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void validate(Object arg0, Errors arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
